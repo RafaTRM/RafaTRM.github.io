d9538526cc4b97cd9d65d541b9bc9eb62f008236
RafaTRM <68725627+RafaTRM@users.noreply.github.com>
2024-07-19 13:54:56 -0600

    Add files via upload

--- scriptcarrusel.js
+++ scriptcarrusel.js
@@ -2,12 +2,12 @@ let next = document.querySelector('.carnext');
 let prev = document.querySelector('.carprev');
 let carslider = document.querySelector('.carslider');
 
-next.addEventListener('click', function(){
+prev.addEventListener('click', function(){
     let carslides = document.querySelectorAll('.carslides');
     carslider.appendChild(carslides[0]);
 });
 
-prev.addEventListener('click', function(){
+next.addEventListener('click', function(){
     let carslides = document.querySelectorAll('.carslides');
     carslider.prepend(carslides[carslides.length - 1]);
 });
