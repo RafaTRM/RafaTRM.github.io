fca9b1f72c7ea6e47b63004ddae07949e99ce1a5
RafaTRM <68725627+RafaTRM@users.noreply.github.com>
2024-08-08 21:33:00 -0600

    Add files via upload

--- xperience.js
+++ xperience.js
@@ -1,73 +1,16 @@
-let slider = document.querySelector(".xperience .slider-contenedor")
-let sliderIndividual = document.querySelectorAll(".contenido-slider")
-let contador = 1;
-let width = sliderIndividual[0].clientWidth;
-let intervalo = 9000;
-
-window.addEventListener("resize", function(){
-    width = sliderIndividual[0].clientWidth;
-})
-
-/******* 
-sliderIndividual.addEventListener('mouseover', () => {
-    stop();
-})
-const stop = () => {
-    clearInterval(intervalo);
-    intervalo = 
-    5000;
-};
-*/
-
-/** 
-
-slider.addEventListener('mouseout', () => {
-    setInterval();
-})
-
-setInterval();
-
-
-/*****/
-
-
-setInterval(function(){
-    slides();
-},intervalo);
-
-function slides() {
-    slider.style.transform = "translate("+(-width*contador)+"px)";
-    slider.style.transition = "transform 1s";
-    contador++;
-    if (contador == sliderIndividual.length) {
-        setTimeout(function(){
-            slider.style.transform = "translate(0px)";
-            slider.style.trasition = "transform 1s";
-            contador = 1;
-        },9000)
-    }
-};
-
-
-
-
-
-/*const industria = document.querySelector(".xperience .slider-contenedor");*/
-/*
-const industria = document.querySelector(".industrias .indcontainer .ind-items");
+const industria = document.querySelector(".ind-items");
 let maxScrollLeft = industria.scrollWidth - industria.clientWidth;
-let intervalo = null;
+let intervalo = 3000;
 let step = 1;
-
 const start = () => {
-    intervalo = setInterval(function() {
+    intervalo = setInterval(function () {
         industria.scrollLeft = industria.scrollLeft + step;
         if (industria.scrollLeft === maxScrollLeft) {
             step = step * -1;
         } else if (industria.scrollLeft === 0) {
             step = step * -1;
         }
-    },7);
+    },18);
 };
 const stop = () => {
     clearInterval(intervalo);
@@ -81,5 +24,60 @@ industria.addEventListener('mouseout', () => {
     start();
 });
 
+
+start();
+
+
+
+
+
+
+
+/*let slider = document.querySelector(".xperience .slider-contenedor")
+let sliderIndividual = document.querySelectorAll(".contenido-slider")
+let contador = 1;
+let width = sliderIndividual[0].clientWidth;
+let intervalo = 3000;
+/*let intervaloE = 190000;
+
+window.addEventListener("resize", function(){
+    width = sliderIndividual[0].clientWidth;
+})
+
+setInterval(function(){
+    start();
+},intervalo)
+
+function start() {
+    slider.style.transform = "translate("+(-width*contador)+"px)";
+    slider.style.transition = "transform 1s";
+    contador++;
+    if (contador == sliderIndividual.length) {
+        setTimeout(function(){
+            slider.style.transform = "translate(0px)";
+            slider.style.trasition = "transform 1s";
+            contador = 1;
+        },9000)
+    }
+};
+
+/*
+const stop = () => {
+    /*clearInterval(intervalo);
+    setTimeout("900000");
+};
+
+sliderIndividual.addEventListener('mouseover', () => {
+    stop();
+})
+
+
+
+
+slider.addEventListener('mouseout', () => {
+    start();
+});
+
+/*
 start();
 */
